bb322295c99cba4e48fa60588bf1c4d3eaedca17
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-11-09 20:54:41 -0300

    Adds a traffic data analysis control

---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,23 @@
+
+import { GA_TRACKING_ID } from '../../lib/gtag';
+
+const Analytics = () => (
+    <>
+        {/* Global site tag (gtag.js) - Google Analytics  */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script
+         dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${GA_TRACKING_ID}, {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
+    </>
+);
+
+export default Analytics;
